b7a51e059087c24da77c57b073db31fb8b63b395
Nada602 <nadaessam602@gmail.com git config --global user.name Nada602>
2025-04-28 13:29:13 +0200

    Updated site content

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-const btn = document.querySelector("button");
-const val = document.getElementById("newTask");
+$(document).ready(function () {
+  const btn = document.querySelector("button");
+  const val = document.getElementById("newTask");
 
-const container = document.querySelector("#tasksContainer");
+  const container = document.querySelector("#tasksContainer");
 
-// document.addEventListener("DOMContentLoaded", loadInDOM);
+  // document.addEventListener("DOMContentLoaded", loadInDOM);
 
-btn.addEventListener("click", function () {
-  const newTask = val.value.trim();
-console.log(newTask);
-  if (newTask != "") {
-    createTasks(newTask);
-    val.value = "";
-  } else {
-    alert("please add new task ");
+  btn.addEventListener("click", function () {
+    const newTask = val.value.trim();
+    console.log(newTask);
+    if (newTask != "") {
+      createTasks(newTask);
+      val.value = "";
+    } else {
+      alert("please add new task ");
+    }
+  });
+
+  function createTasks(newTask) {
+    const li = document.createElement("li");
+    li.textContent = newTask;
+    container.appendChild(li);
+
+    const span = document.createElement("span");
+    span.textContent = "\u00d7";
+    li.appendChild(span);
   }
+
+  $("#tasksContainer").on("click", function (e) {
+    const $target = $(e.target);
+
+    if ($target.is("li")) {
+      $target.toggleClass("checked");
+    } else if ($target.is("span")) {
+      $target.parent().remove();
+    } else if ($target.parent().is("li")) {
+      $target.parent().toggleClass("checked");
+    }
+  });
+  
 });
-
-function createTasks(newTask) {
-
- const li= document.createElement('li');
-li.innerHTML=newTask;
-container.appendChild(li);
-
-const span= document.createElement("span")
-span.innerHTML="\u00d7";
-li.appendChild(span);
-
-}
-
-$("#tasksContainer").on("click", function (e) {
-  const $target = $(e.target);
-
-  if ($target.is("li")) {
-    $target.toggleClass("checked");
-  } else if ($target.is("span")) {
-    $target.parent().remove();
-  } else if ($target.parent().is("li")) {
-    $target.parent().toggleClass("checked");
-  }
-});
-
-
